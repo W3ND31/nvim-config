2aedd62d0f811b6fb5b7c7baa1c51ea51a4cd54d
Wendel Macedo <wendel.macedo@nubank.com.br>
2024-10-10 12:17:07 -0300

    adding new keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
+map("n", "\\", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "|", "<C-W>v", { desc = "Split Window Right", remap = true })
 
 map("n", "<localleader>rs", "<cmd>Lein<CR>", { desc = "REPL Start" })
 
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -134,6 +134,4 @@ return {
       },
     },
   },
-  { "nvchad/volt", lazy = true },
-  { "nvchad/menu", lazy = true },
 }
