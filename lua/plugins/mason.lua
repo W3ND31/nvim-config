d32f3e00ef43a69fdb9e6c9912fa9e28cca57d8c
Wendel Macedo <wendel.macedo@nubank.com.br>
2025-05-09 13:42:00 -0300

    automatizing conform plugins and join it with mason plugins

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,6 @@
 return {
 	{
-		"williamboman/mason.nvim",
+		"mason-org/mason.nvim",
 		build = ":MasonUpdate",
 		cmd = { "Mason", "MasonInstall" },
 		opts = {
@@ -19,27 +19,14 @@ return {
 	{
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		opts = {
-			ensure_installed = {
-				"bash-language-server",
-				"clj-kondo",
-				"clojure-lsp",
-				"cljfmt",
-				"lua-language-server",
-				"vim-language-server",
-				"luacheck",
-				"shellcheck",
-				"shfmt",
-				"stylua",
-				"ast-grep",
-			},
+			ensure_installed = {},
 			auto_update = true,
 			debounce_hours = 5,
 		},
 	},
 	{
-		"williamboman/mason-lspconfig.nvim",
+		"mason-org/mason-lspconfig.nvim",
 		opts = {
-			automatic_installation = true,
 			ensure_installed = {
 				"lua_ls",
 				"clojure_lsp",
@@ -80,5 +67,56 @@ return {
 				css = { "stylelint" },
 			}
 		end,
+	},
+	{
+		"W3ND31/mason-nvim-lint",
+		opts = {
+			automatic_installation = true,
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				clojure = { "cljfmt" },
+				dart = { "ast_grep" },
+				-- beautysh
+				sh = { "beautysh" },
+				zsh = { "beautysh" },
+				--prettier
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				css = { "prettier" },
+			},
+			notify_on_error = true,
+			notify_no_formatters = true,
+		},
+		keys = {
+			{
+				"<leader>cf",
+				function()
+					require("conform").format()
+				end,
+				mode = { "n", "v" },
+				desc = "Format",
+			},
+			{
+				"È", -- Personal keymap because EurKey (Shift + Alt + f => È)
+				function()
+					require("conform").format()
+				end,
+				mode = { "n", "v", "i" },
+				desc = "Format",
+			},
+		},
+	},
+	{
+		"zapling/mason-conform.nvim",
+		opts = {
+			ignore_install = {},
+		},
 	},
 }
