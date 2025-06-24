27dd30602ff59b3ded91d970667d4d24ac4c90d8
Wendel Macedo <wendelmacedooliveira@hotmail.com>
2025-06-24 00:38:59 -0300

    updating options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -36,7 +36,8 @@ vim.filetype.add({
 	pattern = {
 		["${NU_HOME}/.*/resources/.*_config.edn.base"] = "clojure",
 		["${NU_HOME}/.*/resources/.*_config.json.base"] = "json",
-		["${HOME}/.nurc"] = "bash",
+    ["${HOME}/.nurc"] = "zsh",
+    ["${HOME}/stow/nu/.nurc"] = "zsh",
 		["${HOME}/.zshrc"] = "zsh",
 		["${HOME}/.bashrc"] = "bash",
 	},
